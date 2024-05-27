8ede44b9264a7dce1a33329cfd0483b474ff4526
Pablo-Echegaray <pabloechegaray95@gmail.com>
2024-05-27 10:15:52 -0300

    Se agregan ajustes.

--- model/UserModel.php
+++ model/UserModel.php
@@ -11,11 +11,12 @@ class UserModel
     public function registrarUsuario($user, $pass)
     {
         $codigo = self::generarCodigo();
-        $this->database->execute("
-            INSERT INTO usuario(name, pass, code)
-            VALUES ('$user','$pass','$codigo')
-        ");
-        return $this->database->query("SELECT * FROM usuario WHERE code = '$codigo'");
+        $this->database->execute("INSERT INTO usuario(name, pass, codigo) VALUES ('$user','$pass','$codigo')");
+        return $this->getCodigoUsuario($codigo);
+    }
+
+    public function getCodigoUsuario($codigo){
+        return $this->database->query("SELECT * FROM usuario WHERE codigo = '$codigo'");
     }
 
     private static function generarCodigo() {
