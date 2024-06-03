83ef60ceefd06b714131f6c198f0f274ae12c851
regiisan <regiisanchez@hotmail.com>
2024-06-03 17:50:27 -0300

    probando vista

--- model/UserModel.php
+++ model/UserModel.php
@@ -10,10 +10,49 @@ class UserModel
 
     public function obtener($user, $pass)
     {
-        return $this->database->query("
-        SELECT * 
+        return $this->database->query(
+        "SELECT * 
         FROM usuarios
-        WHERE nombre_usuario = '$user' AND password = '$pass'
-        ");
+        WHERE nombre_usuario = '$user' AND password = '$pass'"
+        );
     }
+
+    public function getPregunta($numRandom)
+    {
+
+        return $this->database->query(
+        "SELECT descripcion
+        FROM preguntas
+        WHERE id = 1" //(probando)
+        );
+    }
+
+    public function getRespuestas($numRandom)
+    {
+        return $this->database->query(
+
+            "SELECT p.id, p.descripcion, ri.descripcion 
+            FROM preguntados.preguntas p 
+            INNER JOIN preguntas_respuestas_incorrectas pri ON pri.id_pregunta = p.id 
+            INNER JOIN respuestas_incorrectas ri ON ri.id = pri.id_respuesta_incorrecta 
+            WHERE p.id = 1 
+            UNION SELECT p.id, p.descripcion, rc.descripcion
+            FROM preguntados.preguntas p 
+            INNER JOIN respuestas_correctas rc ON p.id_respuesta_correcta = rc.id 
+            WHERE p.id = 1;
+            "
+        );
+    }
+
+    public function getRespuestaCorrecta($numRandom)
+    {
+        return $this->database->query(
+
+            "SELECT rc.descripcion
+            FROM preguntados.preguntas p 
+            INNER JOIN preguntados.respuestas_correctas rc ON p.id_respuesta_correcta = rc.id
+            WHERE p.id = 1"
+        );
+    }
+
 }
