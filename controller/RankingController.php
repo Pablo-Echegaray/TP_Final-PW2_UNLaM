585e0735345d1a21bd32c63cc4c782f40e464784
Micaela Mendez <micaela.mr03@gmail.com>
2024-06-06 20:21:20 -0300

    Ranking

---
+++ controller/RankingController.php
@@ -0,0 +1,16 @@
+<?php
+class RankingController {
+    private $presenter;
+    private $model;
+
+    public function __construct($model, $presenter)
+    {
+        $this->presenter = $presenter;
+        $this->model = $model;
+    }
+
+    public function mostrarRanking() {
+        $ranking = $this->model->getRanking();
+        $this->presenter->render("view/rankingView.mustache", array("ranking" => $ranking));
+    }
+}
