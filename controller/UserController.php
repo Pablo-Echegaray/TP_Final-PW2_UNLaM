d8008a7bb28a71aa0fc458d9ec4cc4f36827e162
Pablo-Echegaray <pabloechegaray95@gmail.com>
2024-06-20 21:03:10 -0300

    editar pregunta parte 1.

--- controller/UserController.php
+++ controller/UserController.php
@@ -2,12 +2,14 @@
 class UserController
 {
     private $presenter;
-    private $model;
+    private $modelUser;
+    private $modelQuestion;
 
-    public function __construct($model, $presenter)
+    public function __construct($modelUser, $modelQuestion, $presenter)
     {
         $this->presenter = $presenter;
-        $this->model = $model;
+        $this->modelUser = $modelUser;
+        $this->modelQuestion = $modelQuestion;
     }
 
     public function get()
@@ -25,7 +27,7 @@ class UserController
         if (isset($_POST["user"]) && isset($_POST["pass"])) {
             $user = $_POST["user"];
             $pass = $_POST["pass"];
-            $usuario = $this->model->obtener($user, $pass);
+            $usuario = $this->modelUser->obtener($user, $pass);
 
             if ($usuario) {
                 $_SESSION["usuario"] = $usuario;
@@ -48,6 +50,7 @@ class UserController
     private function renderHomeView($usuario)
     {
         $rol = $usuario[0]["rol"];
+        echo $rol;
 
         switch ($rol) {
             case 'J':
@@ -55,11 +58,11 @@ class UserController
                 break;
             case 'E':
                 $estado = "activa";
-                $preguntas = $this->model->getQuestionsAndAnswers($estado);
+                $preguntas = $this->modelQuestion->getQuestionsAndAnswers($estado);
                 $this->presenter->render("view/editorHomeView.mustache", ["usuario" => $usuario, "preguntas" => $preguntas, "activas" => true]);
                 break;
             case 'A':
-                $this->presenter->render("view/adminView.mustache", ["usuario" => $usuario]);
+                $this->presenter->render("view/homeView.mustache", ["usuario" => $usuario]);
                 break;
         }
     }
@@ -84,7 +87,7 @@ class UserController
     public function profile() 
     {
         $id = $_GET['id'];
-        $usuario = $this->model->getUserById($id);
+        $usuario = $this->modelUser->getUserById($id);
         if (isset($id) && $usuario) {
             $this->presenter->render("view/otherUsersView.mustache", array("usuario" => $usuario));
         } else {
@@ -97,7 +100,7 @@ class UserController
     public function approveQuestion()
     {
         $idPregunta = $_POST['preguntaId'];
-        $this->model->approveQuestion($idPregunta);
+        $this->modelQuestion->approveQuestion($idPregunta);
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         exit; 
@@ -106,17 +109,17 @@ class UserController
     public function disapproveQuestion()
     {
         $idPregunta = $_POST['preguntaId'];
-        $this->model->disapproveQuestion($idPregunta);
+        $this->modelQuestion->disapproveQuestion($idPregunta);
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         exit; 
     }
-    
+
     public function deleteQuestion()
     {
         $idPregunta = $_POST['preguntaId'];
-        $this->model->deleteAnswersForQuestion($idPregunta);
-        $this->model->deleteQuestion($idPregunta);
+        $this->modelQuestion->deleteAnswersForQuestion($idPregunta);
+        $this->modelQuestion->deleteQuestion($idPregunta);
 
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         exit; 
@@ -125,35 +128,71 @@ class UserController
     public function editQuestion()
     {
         $idPregunta = isset($_POST['preguntaId']) ? $_POST['preguntaId'] : null;
-        $pregunta = $this->model->getQuestion($idPregunta);
-        $respuestas = $this->model->getAnswers($idPregunta);
-        $categorias =  $this->model->getCategorias();
-        
+        $pregunta = $this->modelQuestion->getQuestion($idPregunta);
+        $respuestas = $this->modelQuestion->getAnswers($idPregunta);
+        $categorias =  $this->modelQuestion->getCategorias();
+
+
         $this->presenter->render("view/editarPreguntaView.mustache", ["pregunta" => $pregunta, "usuario" => $_SESSION['usuario'], "respuestas" => $respuestas, "categorias" => $categorias]);
     }
 
     public function inactiveQuestions()
     {
         $estado = "inactiva";
-        $preguntas = $this->model->getQuestionsAndAnswers($estado);
+        $preguntas = $this->modelQuestion->getQuestionsAndAnswers($estado);
         $this->presenter->render("view/editorHomeView.mustache", ["usuario" =>  $_SESSION["usuario"], "preguntas" => $preguntas, "reportadas" => true]);
     }
 
     public function suggestedQuestions()
     {
         $estado = "sugerida";
-        $preguntas = $this->model->getQuestionsAndAnswers($estado);
+        $preguntas = $this->modelQuestion->getQuestionsAndAnswers($estado);
         $this->presenter->render("view/editorHomeView.mustache", ["usuario" =>  $_SESSION["usuario"], "preguntas" => $preguntas, "sugeridas" => true]);
     }
 
     public function reportedQuestions()
     {
         $estado = "reportada";
-        $preguntas = $this->model->getQuestionsAndAnswers($estado);
+        $preguntas = $this->modelQuestion->getQuestionsAndAnswers($estado);
         $this->presenter->render("view/editorHomeView.mustache", ["usuario" =>  $_SESSION["usuario"], "preguntas" => $preguntas, "reportadas" => true]);
     }
 
 
+    public function updateQuestion(){
+        $idPregunta = isset($_POST['id_pregunta']) ? $_POST['id_pregunta'] : null;
+        $pregunta = isset($_POST['descripcion']) ? $_POST['descripcion'] : null;
+        $idCategoria =  isset($_POST['categoria']) ? $_POST['categoria'] : null;
+        $respuestaIds = isset($_POST['respuestaId']) ? $_POST['respuestaId'] : null;
+        $respuestaDescripciones = isset($_POST['opcion']) ? $_POST['opcion'] : null;
+        $correcta = isset($_POST['correcta']) ? $_POST['correcta'] : null;
+        $answers = [];
 
+
+        echo "id pregunta controller:". $idPregunta . "<br>";
+        /*
+        echo "pregunta controller" . $pregunta . "<br>";
+        echo "categoriaID desde controller" . $categoriaId . "<br>";
+        echo "respuestaID" . $_POST['respuestaId'][0] . "descripcion res" . $_POST['opcion'][0] . "<br>";
+        echo "respuestaID" . $_POST['respuestaId'][1] . "descripcion res" . $_POST['opcion'][1] . "<br>";
+        echo "respuestaID" . $_POST['respuestaId'][2] . "descripcion res" . $_POST['opcion'][2] . "<br>";
+        echo "respuestaID" . $_POST['respuestaId'][3] . "descripcion res" . $_POST['opcion'][3] . "<br>";
+*/
+
+        //echo "respuestaID" . $_POST['respuestaId'][0];
+       // echo "respuestaID" . $_POST['respuestaId'][1];
+        for ($i=0; $i < count($respuestaIds); $i++) {
+            $answers[$i] = ["id" => "$respuestaIds[$i]", "descripcion" => $respuestaDescripciones[$i], "estado" => 0];
+            //echo "respuestaID " . $respuestaIds[$i] . "<br>";
+            //echo "respuestaDescripcion " . $respuestaDescripciones[$i] . "<br>";
+        }
+        /*
+        echo "id respuesta: " . $answers[0]['id'] . "descripcion res: " . $answers[0]['descripcion'] . "<br>";
+        echo "id respuesta: " . $answers[1]['id'] . "descripcion res: " . $answers[1]['descripcion'] . "<br>";
+        echo "id respuesta: " . $answers[2]['id'] . "descripcion res: " . $answers[2]['descripcion'] . "<br>";
+        echo "id respuesta: " . $answers[3]['id'] . "descripcion res: " . $answers[3]['descripcion'] . "<br>";
+        */
+
+        $this->modelQuestion->editQuestionAndAnswers($idPregunta, $idCategoria, $pregunta, $answers, $correcta);
+    }
 
 }
