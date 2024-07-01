3a2158a8ad82d67fe8d430054cd7533832d76296
Pablo2810 <pablorocha.2810@gmail.com>
2024-07-01 16:27:42 -0300

    Refresh y vista de usuarios del ranking

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -16,7 +16,18 @@ class PartidaController
             header('Location: http://localhost/TP_Final-PW2_UNLaM/user/get');
             exit();
         }
+
         $usuario = $_SESSION["usuario"];
+
+        // Verifica si la página fue recargada
+        /*if (isset($_SESSION['page_reloaded']) && $_SESSION['page_reloaded'] === true) {
+            $this->finishGame();
+            return;
+        }*/
+
+        // Marca la página como recargada
+        //$_SESSION['page_reloaded'] = true;
+
         $game = $this->model->playTheGame($usuario);
         $this->presenter->render("view/jugarView.mustache", $game);
     }
@@ -32,9 +43,12 @@ class PartidaController
         $usuario = $_SESSION["usuario"];
         $validatedQuestion = $this->model->checkAnswer($usuario, $respuestaUsuario);
         $actionGame = $validatedQuestion['actionGame'];
+
+        // Resetea el indicador de recarga de página cuando se responde una pregunta
+        //$_SESSION['page_reloaded'] = false;
+
         $this->presenter->render("view/mensajePartidaView.mustache", $validatedQuestion);
         header('Refresh: 2; URL=/TP_Final-PW2_UNLaM/partida/' . $actionGame);
-
     }
 
     public function finishGame()
@@ -43,9 +57,14 @@ class PartidaController
             header('Location: http://localhost/TP_Final-PW2_UNLaM/user/get');
             exit();
         }
+
         $lastquestion = $this->model->getLastQuestionInGame();
-        $puntaje=$this->model->getPuntajeJugadorEnPartida($lastquestion["id_partida"]);
-        $this->presenter->render("view/finalizarPartidaView.mustache", ["puntaje"=>$puntaje]);
+        $puntaje = $this->model->getPuntajeJugadorEnPartida($lastquestion["id_partida"]);
+
+        // Resetea el indicador de recarga de página al finalizar el juego
+        //unset($_SESSION['page_reloaded']);
+
+        $this->presenter->render("view/finalizarPartidaView.mustache", ["puntaje" => $puntaje]);
     }
 
     public function timerRefresh()
@@ -54,8 +73,12 @@ class PartidaController
             header('Location: http://localhost/TP_Final-PW2_UNLaM/user/get');
             exit();
         }
+
         $score_and_error = $this->model->timerRefresh();
+
+        //Resetea el indicador de recarga de página al finalizar el juego
+        //unset($_SESSION['page_reloaded']);
+
         $this->presenter->render("view/finalizarPartidaView.mustache", $score_and_error);
     }
-
 }
