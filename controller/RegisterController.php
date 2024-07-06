fe41f648b6f5963ba3c33d460a0a41106f7b1545
Pablo-Echegaray <pabloechegaray95@gmail.com>
2024-07-05 23:19:05 -0300

    Refactor RegisterController.

--- controller/RegisterController.php
+++ controller/RegisterController.php
@@ -24,63 +24,19 @@ class RegisterController
         $pais = $_POST["pais"] ?? "";
         $ciudad = $_POST["ciudad"] ?? "";
         $email = $_POST["mail"] ?? "";
-        $contrasena = self::validarContrasena($_POST["password"], $_POST["repeat_password"]);
-        $usuario = self::validarUsuario($_POST["username"]);
+        $contrasena = $this->model->validarContrasena($_POST["password"], $_POST["repeat_password"]);
+        $usuario = $this->model->validarUsuario($_POST["username"]);
         $foto = $this->model->verificarImagen($_FILES["perfil"]);
         $lat = $_POST["lat"] ?? "";
         $lng = $_POST["lng"] ?? "";
-        $rol = "J";
 
-        if ($contrasena != null) {
-            if ($usuario != null) {
-                //AGREGA AL USUARIO COMO NO ACTIVO y POR DEFECTO COMO JUGADOR
-                $this->model->agregar($nombre, $apellido, $nacimiento, $sexo, $ciudad, $pais, $email, $contrasena, $usuario, $foto);
-                $this->model->saveCoordinates($usuario, $ciudad, $pais, $lat, $lng);
-                $this->model->enviarCorreoVerificacion($email, $nombre, $usuario);
-
-                $mensajeVerificacion = "Verifica tu correo para iniciar sesion";
-                $this->presenter->render("view/iniciarSesionView.mustache", ["mensaje" => $mensajeVerificacion]);
-            } else {
-                $error = "El nombre de usuario ya existe";
-                $this->presenter->render("view/registrarseView.mustache", ["error" => $error]);
-            }
-        } else {
-            $error = "Las contraseñas no coinciden";
-            $this->presenter->render("view/registrarseView.mustache", ["error" => $error]);
-        }
+        $data = $this->model->addUser($nombre, $apellido, $nacimiento, $sexo, $ciudad, $pais, $email, $contrasena, $usuario, $foto, $lat, $lng);
+        $this->presenter->render("view/".$data[0]."View.mustache", $data[1]);
     }
 
     private function getMaps(){
         $listMaps = $this->model->getMaps();
         //echo $listMaps[0][0];
         return $listMaps;
-    }
-
-    private function validarNacimiento($nacimiento)
-    {
-        if (isset($nacimiento)) {
-            return $this->model->calcularEdad($nacimiento);
-        }
-        return "";
-    }
-
-    private function validarContrasena($contrasena, $contrasenaRepetida)
-    {
-        if (isset($contrasena) && isset($contrasenaRepetida)){
-            if ($this->model->verificarContrasena($contrasena, $contrasenaRepetida)){
-                return $contrasena;
-            } else{ return null; }
-        }
-        return null;
-    }
-
-    private function validarUsuario($username)
-    {
-        if (isset($username)) {
-            if ($this->model->verificarUsername($username)) {
-                return $username;
-            } else { return null; }
-        }
-        return null;
     }
 }
